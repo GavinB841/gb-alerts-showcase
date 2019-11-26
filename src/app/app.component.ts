2e69d51d62d197510852ad36884a0971e2a7fba9
Gavin Boyle <gboyle@nightline-delivers.com>
2019-11-26 13:26:32 +0000

    Add project library files and showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { GbAlertsService } from 'gb-alerts';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h2>Simple Notifications</h2>
+    <div class="simple-notifications">
+      <button class="success-angle" (click)="createSuccessAlert()">Create success alert</button>
+      <button class="danger-shadow" (click)="createDangerAlert()">Create danger alert</button>
+      <button class="warning-gradient" (click)="createWarningAlert()">Create warning alert</button>
+      <button class="info-pulse" (click)="createInfoAlert()">Create info alert</button>
+      <gb-alerts></gb-alerts>
+    </div>
+  `,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gb-alerts-showcase';
+  constructor(private gbAlertsService: GbAlertsService) {}
+
+  public createSuccessAlert(): void {
+    this.gbAlertsService.createSuccessAlert('Successfully Created');
+  }
+
+  public createDangerAlert(): void {
+    this.gbAlertsService.createDangerAlert('Danger Cannot Delete');
+  }
+
+  public createWarningAlert(): void {
+    this.gbAlertsService.createWarningAlert('Warning Do Not Refresh');
+  }
+
+  public createInfoAlert(): void {
+    this.gbAlertsService.createInfoAlert('Include This Field');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GbAlertsModule } from 'gb-alerts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GbAlertsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
